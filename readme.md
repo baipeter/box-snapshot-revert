e61c80c07b98bb57d775984a27fb3da426388bd9
Peter Bai <peter@peterbai.com>
2014-06-29 22:14:16 -0700

    First level subfolder seeking

---
+++ readme.md
@@ -0,0 +1,1 @@
+Use `export BOX_ACCESS_TOKEN=MYTOKEN` in terminal to set environment variable.
--- ss-revert-01.py
+++ ss-revert-01.py
@@ -3,19 +3,54 @@ import json
 import os
 from sys import exit
 
+
+def get_folder_contents(folderid):
+
+    url = 'https://api.box.com/2.0/folders/' + str(folderid) + '/items'
+    payload = ''
+    headers = {
+        'Authorization': 'Bearer ' + token
+    }
+
+    r = requests.get(url, headers = headers)
+    response = json.loads(r.text)
+    # print json.dumps(response, indent=4)
+
+    return response
+
+
+# Load access token
 token = os.environ.get('BOX_ACCESS_TOKEN')
 if token == None:
     print 'No token defined in environment. Exiting.'
     exit(1)
 
-# Retrieve all items in root folder
 
-url = 'https://api.box.com/2.0/folders/0/items'
-payload = ''
-headers = {
-    'Authorization': 'Bearer ' + token
-}
+# Get root folder items
+response = get_folder_contents(0)
 
-r = requests.get(url, headers = headers)
-response = json.loads(r.text)
-print json.dumps(response, indent=4)
+
+# Process response
+folders = []
+
+for element in response['entries']:
+    if element['type'] == 'folder':
+        folders.append(element['id'])
+
+
+# Get individual folder items
+subFolders = []
+
+for item in folders:
+    print 'Getting contents of folder', item
+    response = get_folder_contents(item)
+
+    for element in response['entries']:
+        if element['type'] == 'folder':
+            subFolders.append(element['id'])
+
+if subFolders:
+    folders.extend(subFolders)
+
+
+print folders
